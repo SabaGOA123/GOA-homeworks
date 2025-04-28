f6d88b5459269d131b08a5df24db05983ccad104
SabaGOA123 <sabagurash@gmail.com>
2025-04-28 12:31:02 -0700

    commit

---
+++ Day 01/homework/homework.py
@@ -0,0 +1,9 @@
+# 3) შექმენით ცვლადები სადაც შეინახავთ ინფორმაციას თქვენს თავზე მაგ(name , age , address , height , weight) და ტერმინალში გამოიტანეთ სრული ინფორმაცია
+
+name = "saba gurashvili"
+age = "14"
+height =" 1.67"
+weight = "67"
+address = "sayviris qucha N2"
+
+print("chemi saxeli aris " + name + " var " + age + " wlis, " + "simagleshi var " + height + " santimetri, " + "da " + weight + " kilogrami, " + "me vcxovrob " + address + "shi")
---
+++ Day 01/homework/homework3.py
@@ -0,0 +1,5 @@
+# 5)კომენტარების გამოყენებით , სათითაოდ ჩამოწერეთ გაკვეთილზე ნასწავლი მონაცემთა ტიპები , მოიყვანეთ მაგალითები და ასევე დაბეჭდეთ ტერმინალში თითოეული
+
+# stringi-nebismieri aso,wiladi,cifrebi,da ase shemdeg yvelaferi rac aris moqceuli prjyalebshi
+# integeri-mteli ricxvebi
+# floati-atwiladebi
