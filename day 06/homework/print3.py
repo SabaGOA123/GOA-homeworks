8ce00643f22ef9119bda887154a20b1e88f21d83
SabaGOA123 <sabagurash@gmail.com>
2025-06-11 07:47:24 -0700

    commit

---
+++ day 06/homework/print3.py
@@ -0,0 +1,11 @@
+# შექმენი 5 ცვლადი number1 , number2...number5 ამ ცვლადებში მომხმარებელს შემოატანინე რიცხვები და შემდეგ average-ცვლადში გამოთვალე ამ რიცხვების საშუალო არითმეტიკული და ბოლოს დაბეჭდე საბოლოო შედეგი
+
+number1 = (input("30"))
+number2 = (input("3"))
+number3 = (input("2"))
+number4 = (input("5"))
+number5 = (input("15"))
+
+average = (number1 + number2 + number3 + number4 + number5)
+
+print("შეყვანილი რიცხვების საშუალო არითმეტიკულია:", average)
---
+++ day 06/homework/print4.py
@@ -0,0 +1,18 @@
+# მომხმარებელს შემოატანინე 2 რიცხვი ეს რიცხვები შეინახე 2 განსხვავებულ ცვლადში (num1 , num2) და შეასრულე ამ რიცხვებზე ყველა მათემატიკური ოპერაცია რაც გვაქვს გავლილი
+
+num1 = (input("10"))
+num2 = (input("5"))
+
+sum_result = num1 + num2         # შეკრება
+difference = num1 - num2         # გამოკლება
+product = num1 * num2            # გამრავლება
+division = num1 / num2           # გაყოფა
+modulus = num1 % num2            # ნაშთი გაყოფისას
+power = num1 ** num2             # ხარისხში აყვანა
+
+print("შეკრება:", sum_result)
+print("გამოკლება:", difference)
+print("გამრავლება:", product)
+print("გაყოფა:", division)
+print("ნაშთი გაყოფისას:", modulus)
+print("ხარისხში აყვანა:", power)
